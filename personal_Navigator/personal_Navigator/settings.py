1b386da385e147879b3102139a09bf366881cec6
Aaditya Kumar <127669174+GitAaditya@users.noreply.github.com>
2024-03-19 20:51:38 +0530

    Update settings.py
    update the secret key with your own one.

--- personal_Navigator/personal_Navigator/settings.py
+++ personal_Navigator/personal_Navigator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*^ot1qr*9m520fhm+0#+$s@ogj%$c_izozf+*q6o!rttqik-t'
+SECRET_KEY = 'django-insecure-YOUR-SECRET-KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
